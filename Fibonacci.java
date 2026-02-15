b9972ae38cb481fcaca5762fe6c59840e63706b3
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2026-02-15 23:58:05 +0530

    create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,29 @@
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        if (n == 0) {
+            System.out.println(0);
+        }
+
+        else if (n == 1) {
+            System.out.println("0 1");
+        }
+ 
+        else {
+            int[] fib = new int[n + 1];  // Array to hold Fibonacci numbers
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            System.out.println("The Fibonacci Series up to " + n + "th term:");
+            for (int i = 0; i <= n; i++) {
+                System.out.print(fib[i] + " ");
+            }
+        }
+    }
+}
