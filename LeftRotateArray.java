3e62d12f000c34ada71896f09849db60675bd4af
pranavkale714 <pranavkale44@gmail.com>
2026-03-20 23:55:35 +0530

    left rotate array by one

---
+++ LeftRotateArray.java
@@ -0,0 +1,26 @@
+class LeftRotateArray {
+
+    public static void solve(int[] arr, int n) {
+        int[] temp = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            temp[i - 1] = arr[i];
+        }
+
+        temp[n - 1] = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(temp[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = {1, 2, 3, 4, 5};
+
+        LeftRotateArray.solve(arr, n);
+    }
+}
