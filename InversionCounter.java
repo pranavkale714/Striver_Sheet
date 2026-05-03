93749cca7f594e784dbaeb0b86e6891177a7e128
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2026-05-03 22:16:24 +0530

    Implement InversionCounter using merge sort

---
+++ InversionCounter.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class InversionCounter {
+
+    public int merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+
+        int left = low;
+        int right = mid + 1;
+        int k = 0;
+
+        int cnt = 0;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[k++] = arr[left++];
+            } else {
+                temp[k++] = arr[right++];
+                cnt += (mid - left + 1);
+            }
+        }
+
+        while (left <= mid) {
+            temp[k++] = arr[left++];
+        }
+
+        while (right <= high) {
+            temp[k++] = arr[right++];
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
+
+        return cnt;
+    }
+
+    public int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+
+        if (low >= high) return cnt;
+
+        int mid = (low + high) / 2;
+
+        cnt += mergeSort(arr, low, mid);
+        cnt += mergeSort(arr, mid + 1, high);
+        cnt += merge(arr, low, mid, high);
+
+        return cnt;
+    }
+
+    public int numberOfInversions(int[] arr) {
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {5, 4, 3, 2, 1};
+
+        InversionCounter obj = new InversionCounter();
+
+        int cnt = obj.numberOfInversions(a);
+
+        System.out.println("The number of inversions are: " + cnt);
+    }
+}
