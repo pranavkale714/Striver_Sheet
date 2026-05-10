0ac4b61f98d049144d09d65e4a15dd4ebdb2f002
pranavkale714 <pranavkale44@gmail.com>
2026-05-10 23:22:20 +0530

    adding file

--- LowerBoundFinder.java
+++ LowerBoundFinder.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
 class LowerBoundFinder {
-    // Function to find the lower bound index
+
     public int lowerBound(int[] arr, int x) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= x) {
-                return i;  // First index where element >= x
+                return i;
             }
         }
-        return arr.length;  // If x is greater than all elements
+        return arr.length;
     }
 
     public static void main(String[] args) {
         int[] arr = {3, 5, 8, 15, 19};
+    }
+}
