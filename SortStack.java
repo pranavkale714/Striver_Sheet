9986568f3f4a48db88a55181f54b962e504a973f
Pranav Kale <96164632+pranavkale714@users.noreply.github.com>
2026-09-03 23:50:53 +0530

    Add recursive stack sorting implementation
    Implement sorting of a stack using recursion.

---
+++ SortStack.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+class Solution {
+
+    public void insert(Stack<Integer> stack, int temp) {
+        if (stack.isEmpty() || stack.peek() <= temp) {
+            stack.push(temp);
+            return;
+        }
+
+        int val = stack.pop();
+
+        insert(stack, temp);
+
+        stack.push(val);
+    }
+
+    public void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int temp = stack.pop();
+
+            sortStack(stack);
+
+            insert(stack, temp);
+        }
+    }
+}
+
+public class SortStack {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(4);
+        stack.push(1);
+        stack.push(3);
+        stack.push(2);
+
+        sol.sortStack(stack);
+
+        System.out.print("Sorted stack (descending order): ");
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
